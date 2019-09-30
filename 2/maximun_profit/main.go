ae9eac53a2033e33024235d1b9e971f8fc12e2c3
pomadev <y.pomadev@gmail.com>
2019-09-30 23:29:00 +0900

    2.5 導入問題実装

---
+++ 2/maximun_profit/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextLine())
+	return i
+}
+
+func main() {
+	n := nextInt()
+
+	r := make([]int, n)
+	for i := range r {
+		r[i] = nextInt()
+	}
+
+	maxv, minv := -2000000000, r[0]
+	for i := 1; i < n; i++ {
+		if r[i] - minv > maxv {
+			maxv = r[i]-minv
+		}
+		if r[i] < minv {
+			minv = r[i]
+		}
+	}
+
+	fmt.Println(maxv)
+}
