32574bdafc44028c663c97b09e8559a54f9a76cb
pomadev <y.pomadev@gmail.com>
2019-10-08 01:27:13 +0900

    4.3 キュー実装

---
+++ 4/queue/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type process struct {
+	name string
+	time int
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+var (
+	head, tail, n int
+	p             []process
+)
+
+func enqueue(x process) {
+	p[tail] = x
+	tail = (tail + 1) % (n + 1)
+}
+
+func dequeue() process {
+	x := p[head]
+	head = (head + 1) % (n + 1)
+	return x
+}
+
+func main() {
+	var t int
+	fmt.Scan(&n, &t)
+
+	p = make([]process, n+1)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&p[i].name, &p[i].time)
+	}
+	head, tail = 0, n
+
+	elaps := 0
+	for head != tail {
+		u := dequeue()
+		c := min(t, u.time)
+		u.time -= c
+		elaps += c
+		if u.time > 0 {
+			enqueue(u)
+			continue
+		}
+		fmt.Printf("%s %d\n", u.name, elaps)
+	}
+}
