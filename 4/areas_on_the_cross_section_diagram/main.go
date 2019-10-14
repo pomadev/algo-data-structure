ac5102fc84ce3845ea6a1d01853b5bd9e6952816
pomadev <y.pomadev@gmail.com>
2019-10-14 23:22:33 +0900

    4.6 実装

--- 4/areas_on_the_cross_section_diagram/main.go
+++ 4/areas_on_the_cross_section_diagram/main.go
@@ -19,6 +19,25 @@ func (s *Stack) Pop() int {
 	return s.s[s.top+1]
 }
 
+type Pair struct {
+	first, second int
+}
+
+type Stack2 struct {
+	p   []Pair
+	top int
+}
+
+func (s2 *Stack2) Push(p Pair) {
+	s2.top++
+	s2.p[s2.top] = p
+}
+
+func (s2 *Stack2) Pop() Pair {
+	s2.top--
+	return s2.p[s2.top+1]
+}
+
 func main() {
 	var l string
 	fmt.Scan(&l)
@@ -26,17 +45,33 @@ func main() {
 	s := new(Stack)
 	s.s = make([]int, len(l))
 	s.top = -1
+	s2 := new(Stack2)
+	s2.p = make([]Pair, len(l))
+	s2.top = -1
 
 	sum := 0
 	for i, v := range l {
-		fmt.Println(s.s, s.top)
 		if v == '\\' {
 			s.Push(i)
 		} else if v == '/' && s.top >= 0 {
 			j := s.Pop()
 			sum += i - j
+			a := i - j
+			for s2.top >= 0 && s2.p[s2.top].first > j {
+				a += s2.Pop().second
+			}
+			s2.Push(Pair{j, a})
 		}
 	}
 
 	fmt.Println(sum)
+	var ans []int
+	for s2.top >= 0 {
+		ans = append(ans, s2.Pop().second)
+	}
+	fmt.Print(len(ans))
+	for i := len(ans) - 1; i >= 0; i-- {
+		fmt.Print(" ", ans[i])
+	}
+	fmt.Println()
 }
