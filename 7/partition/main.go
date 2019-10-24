f77c54e97e69a81e104848469770b853561bb1b7
pomadev <y.pomadev@gmail.com>
2019-10-24 23:29:43 +0900

    7.2 パーティション実装

--- 7/partition/main.go
+++ 7/partition/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func partition(a []int, p, r int) int {
+	x := a[r]
+	i := p - 1
+	for j := p; j <= r-1; j++ {
+		if a[j] <= x {
+			i++
+			a[i], a[j] = a[j], a[i]
+		}
+	}
+	a[i+1], a[r] = a[r], a[i+1]
+	return i + 1
+}
+
 func main() {
 	var n int
 	fmt.Scan(&n)
@@ -9,4 +22,20 @@ func main() {
 	for i := range a {
 		fmt.Scan(&a[i])
 	}
+
+	q := partition(a, 0, n-1)
+
+	for i, v := range a {
+		if i > 0 {
+			fmt.Print(" ")
+		}
+		if i == q {
+			fmt.Print("[")
+		}
+		fmt.Print(v)
+		if i == q {
+			fmt.Print("]")
+		}
+	}
+	fmt.Println()
 }
