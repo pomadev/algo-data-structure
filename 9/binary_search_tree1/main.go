c8e80e84359bedfd4d86924b61a40bba02d6cc01
pomadev <y.pomadev@gmail.com>
2019-11-10 23:42:22 +0900

    9.2 仮実装

--- 9/binary_search_tree1/main.go
+++ 9/binary_search_tree1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -26,15 +27,65 @@ type Node struct {
 var (
 	n         int
 	root, pre *Node
-	T         []Node
 )
 
 func insert(k int) {
+	y, x := pre, root
+	z := &Node{
+		key: k,
+	}
+	for x != nil {
+		y = x
+		if z.key < x.key {
+			x = x.left
+		} else {
+			x = x.right
+		}
+	}
+	z.parent = y
+	if y == nil {
+		root = z
+	} else {
+		if z.key < y.key {
+			y.left = z
+		} else {
+			y.right = z
+		}
+	}
+}
 
+func preOrder(u *Node) {
+	if u == nil {
+		return
+	}
+	fmt.Printf(" %d", u.key)
+	preOrder(u.left)
+	preOrder(u.right)
+}
+
+func inOrder(u *Node) {
+	if u == nil {
+		return
+	}
+	inOrder(u.left)
+	fmt.Printf(" %d", u.key)
+	inOrder(u.right)
 }
 
 func main() {
 	scanner.Split(bufio.ScanWords)
 
 	n = nextInt()
+
+	for i := 0; i < n; i++ {
+		s := nextStr()
+		if s == "insert" {
+			insert(nextInt())
+		} else {
+			inOrder(root)
+			fmt.Println()
+			preOrder(root)
+			fmt.Println()
+		}
+	}
 }
