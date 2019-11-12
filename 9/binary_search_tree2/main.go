61d1390ab26df179b42afac637a861c871dcc892
pomadev <y.pomadev@gmail.com>
2019-11-12 23:47:16 +0900

    9.3 二分探索木：探索実装

---
+++ 9/binary_search_tree2/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var scanner = bufio.NewScanner(os.Stdin)
+
+func nextStr() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func nextInt() int {
+	n, _ := strconv.Atoi(nextStr())
+	return n
+}
+
+type Node struct {
+	key                 int
+	parent, left, right *Node
+}
+
+var root, NIL *Node
+
+func insert(k int) {
+	z := &Node{
+		key: k,
+	}
+	x, y := root, NIL
+	for x != nil {
+		y = x
+		if z.key < x.key {
+			x = x.left
+		} else {
+			x = x.right
+		}
+	}
+	z.parent = y
+	if y == NIL {
+		root = z
+	} else {
+		if z.key < y.key {
+			y.left = z
+		} else {
+			y.right = z
+		}
+	}
+}
+
+func find(u *Node, k int) *Node {
+	for u != NIL && k != u.key {
+		if k < u.key {
+			u = u.left
+		} else {
+			u = u.right
+		}
+	}
+	return u
+}
+
+var b bytes.Buffer
+
+func preOrder(u *Node) {
+	if u == nil {
+		return
+	}
+	b.Write([]byte(fmt.Sprintf(" %d", u.key)))
+	preOrder(u.left)
+	preOrder(u.right)
+}
+
+func inOrder(u *Node) {
+	if u == nil {
+		return
+	}
+	inOrder(u.left)
+	b.Write([]byte(fmt.Sprintf(" %d", u.key)))
+	inOrder(u.right)
+}
+
+func main() {
+	scanner.Split(bufio.ScanWords)
+
+	n := nextInt()
+
+	for i := 0; i < n; i++ {
+		s := nextStr()
+		if s == "insert" {
+			insert(nextInt())
+		} else if s == "find" {
+			x := nextInt()
+			t := find(root, x)
+			if t != NIL {
+				fmt.Println("yes")
+			} else {
+				fmt.Println("no")
+			}
+		} else {
+			inOrder(root)
+			b.WriteTo(os.Stdout)
+			fmt.Println()
+			preOrder(root)
+			b.WriteTo(os.Stdout)
+			fmt.Println()
+		}
+	}
+}
