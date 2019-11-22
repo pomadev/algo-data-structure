6558090a86c379f6f6aae1c75fe5c03cfb1de23b
pomadev <y.pomadev@gmail.com>
2019-11-23 01:27:39 +0900

    11.4 連鎖行列積実装

--- 11/matrix_chain_multiplication/main.go
+++ 11/matrix_chain_multiplication/main.go
@@ -1,12 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var n int
 	fmt.Scan(&n)
-	r, c := make([]int, 6), make([]int, 6)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&r[i], &c[i])
+	p, m := make([]int, n+1), make([][]int, n+1)
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&p[i-1], &p[i])
 	}
+
+	for i := 1; i <= n; i++ {
+		m[i] = make([]int, n+1)
+	}
+	for l := 2; l <= n; l++ {
+		for i := 1; i <= n-l+1; i++ {
+			j := i + l - 1
+			m[i][j] = math.MaxInt64
+			for k := i; k <= j-1; k++ {
+				m[i][j] = min(m[i][j], m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j])
+			}
+		}
+	}
+
+	fmt.Println(m[1][n])
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
